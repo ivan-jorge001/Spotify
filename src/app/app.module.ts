2a3581869683cf9d5a7953b10eb35eaf368baba7
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-06-26 22:46:34 -0400

    spotify

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import { Routes,RouterModule  } from '@angular/router';
+import {LocationStrategy,HashLocationStrategy,APP_BASE_HREF} from '@angular/common';
 
 import { AppComponent } from './app.component';
+import { AlbumComponent } from './album/album.component';
+import { ArtistComponent } from './artist/artist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule
